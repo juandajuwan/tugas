0a3f69f68d57300459028062fa728fa172d55524
juandajuwan <87428278+juandajuwan@users.noreply.github.com>
2021-07-31 10:48:06 +0700

    Add files via upload

---
+++ soal3.js
@@ -0,0 +1,22 @@
+
+
+//var nilaiSantri = [ 52, 95, 55, 67, 79, 44, 30, 97, 84 ];
+// var total = 0;
+// for(var i = 0; i<nilaiSantri.length; i++){
+//     total +=nilaiSantri[i]
+// }
+// console.log(total/nilaiSantri.length);
+
+function hitungVolumeDanLuasPermukanBalok(panjang, lebar){
+         luas= panjang * lebar
+         document.write("panjang : " + panjang + "<br/>");
+         document.write("lebar : " + lebar + "<br/>");
+         document.write("luas persegi panjang : " + luas +"</br>");
+       }
+     panjang=prompt("masukkan nilai panjang" );
+     lebar=prompt("masukkan nilai lebar");
+     hitunglahluaspersegipanjang(panjang, lebar)
+
+
+    
+    
